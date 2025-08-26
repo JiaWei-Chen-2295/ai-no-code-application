fa3fbb13f06f14f35f22b72448561b2fb37a44e1
JiaWei-Chen-2295 <1601020332@qq.com>
2025-08-26 21:41:39 +0800

    init 空项目

---
+++ src/test/java/fun/javierchen/ainocodeapplication/AiNoCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package fun.javierchen.ainocodeapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiNoCodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
